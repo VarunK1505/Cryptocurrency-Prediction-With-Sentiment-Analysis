6f3fce18e9f1277931bbbbe39f895b7e69072d67
Nihal Srivastava <82692408+Nihal-Srivastava05@users.noreply.github.com>
2022-04-11 22:12:06 +0530

    Added prediction viz to All coins.

--- deployment/app.py
+++ deployment/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pickle
 import numpy as np
+import pandas as pd
+from datetime import timedelta
+import statsmodels
 
 app = Flask(__name__)
 
@@ -9,14 +12,72 @@ app = Flask(__name__)
 def home_page():
     return render_template('home.html')
 
-@app.route('/bitcoin')
+@app.route('/bitcoin', methods=['GET', 'POST'])
 def bitcoin_page():
+    if request.method == 'POST':
+        time_frame = request.form.get('time_frame')
+        start_date = request.form.get('start')
+        end_date = request.form.get('end')
+        if(time_frame == 'daily'):
+            dates = pd.date_range(pd.to_datetime(start_date), pd.to_datetime(end_date),freq='d')
+            pickle_in = open("../models/bitcoin_daily.pkl", 'rb')
+            daily_model = pickle.load(pickle_in)
+            pickle_in.close()
+            pred = daily_model.predict(start=start_date, end=end_date)
+        elif(time_frame == 'monthly'):
+            dates = pd.date_range(pd.to_datetime(start_date), pd.to_datetime(end_date),freq='m')
+            pickle_in = open("../models/bitcoin_monthly.pkl", 'rb')
+            monthly_model = pickle.load(pickle_in)
+            pickle_in.close()
+            pred = monthly_model.predict(start=start_date, end=end_date)
+        
+        return render_template('bitcoin.html', check=True ,start_date = start_date, end_date = end_date, labels = dates, predictions = pred)
+
     return render_template('bitcoin.html')
 
-@app.route('/ethereum')
+@app.route('/ethereum', methods=['GET', 'POST'])    
 def ethereum_page():
+    if request.method == 'POST':
+        time_frame = request.form.get('time_frame')
+        start_date = request.form.get('start')
+        end_date = request.form.get('end')
+        if(time_frame == 'daily'):
+            dates = pd.date_range(pd.to_datetime(start_date), pd.to_datetime(end_date),freq='d')
+            pickle_in = open("../models/ethereum_daily.pkl", 'rb')
+            daily_model = pickle.load(pickle_in)
+            pickle_in.close()
+            pred = daily_model.predict(start=start_date, end=end_date)
+        elif(time_frame == 'monthly'):
+            dates = pd.date_range(pd.to_datetime(start_date), pd.to_datetime(end_date),freq='m')
+            pickle_in = open("../models/ethereum_monthly.pkl", 'rb')
+            monthly_model = pickle.load(pickle_in)
+            pickle_in.close()
+            pred = monthly_model.predict(start=start_date, end=end_date)
+        
+        return render_template('ethereum.html', check=True ,start_date = start_date, end_date = end_date, labels = dates, predictions = pred)
+
     return render_template('ethereum.html')
 
-@app.route('/litecoin')
+@app.route('/litecoin', methods=['GET', 'POST'])
 def litecoin_page():
+    if request.method == 'POST':
+        time_frame = request.form.get('time_frame')
+        start_date = request.form.get('start')
+        end_date = request.form.get('end')
+        if(time_frame == 'daily'):
+            dates = pd.date_range(pd.to_datetime(start_date), pd.to_datetime(end_date),freq='d')
+            pickle_in = open("../models/litecoin_daily.pkl", 'rb')
+            daily_model = pickle.load(pickle_in)
+            pickle_in.close()
+            pred = daily_model.predict(start=start_date, end=end_date)
+        elif(time_frame == 'monthly'):
+            dates = pd.date_range(pd.to_datetime(start_date), pd.to_datetime(end_date),freq='m')
+            pickle_in = open("../models/litecoin_monthly.pkl", 'rb')
+            monthly_model = pickle.load(pickle_in)
+            pickle_in.close()
+            pred = monthly_model.predict(start=start_date, end=end_date)
+
+        return render_template('litecoin.html', check=True ,start_date = start_date, end_date = end_date, labels = dates, predictions = pred)
+
+
     return render_template('litecoin.html')
